03c6a28d4e75851e2827d13e3fcc27c97c76cd87
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-05-23 19:14:58 +0200

    Initial commit

---
+++ src/main/java/com/sae201/timeline/Main.java
@@ -0,0 +1,7 @@
+package com.sae201.timeline;
+
+public class Main {
+    public static void main(String[] args) {
+        TimelineApp.main(args);
+    }
+}
---
+++ src/main/java/com/sae201/timeline/TimelineApp.java
@@ -0,0 +1,7 @@
+package com.sae201.timeline;
+
+public class TimelineApp {
+    public static void main(String[] args) {
+
+    }
+}
