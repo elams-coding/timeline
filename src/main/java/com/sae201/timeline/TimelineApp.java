03280cbd3c895a611bbf2523fd40a5f2454d8fa1
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-05-24 13:37:07 +0200

    feat: gérer le lancement de l'application

--- src/main/java/com/sae201/timeline/TimelineApp.java
+++ src/main/java/com/sae201/timeline/TimelineApp.java
@@ -1,7 +1,34 @@
 package com.sae201.timeline;
 
-public class TimelineApp {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+import static com.sae201.timeline.utilitaire.DialogueUtilitaire.alerterChargementVueImpossible;
+
+public class TimelineApp extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        String cheminVueAccueil = "/com/sae201/timeline/accueil.fxml";
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(cheminVueAccueil));
+        try {
+            Scene scene = new Scene(loader.load());
+            stage.setTitle("Timeline");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            if (e.getCause() != null) {
+                alerterChargementVueImpossible(e.getCause().getMessage());
+            } else {
+                alerterChargementVueImpossible(e.getMessage());
+            }
+        }
     }
 }
