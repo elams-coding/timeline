ffdce8d01d9c827141f24d536976a3f9292a30ca
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-05-27 15:32:15 +0200

    création de la méthode afin d'ouvrir la pop-up (en cours)

--- src/main/java/com/sae201/timeline/controller/NombreDeJoueur.java
+++ src/main/java/com/sae201/timeline/controller/NombreDeJoueur.java
@@ -3,6 +3,7 @@ package com.sae201.timeline.controller;
 import static com.sae201.timeline.util.StyleUtilitaire.styleBouton;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
@@ -10,12 +11,16 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class NombreDeJoueur {
+	private static int nbJoueur;
     @FXML
     private VBox root;
+    @FXML
+    private Button unJoueur, deuxJoueurs;
 
     @FXML
     private void initialize() {
         Platform.runLater(this::styleButton);
+        nbJoueur = 0;
     }
 
     private void styleButton() {
@@ -28,5 +33,29 @@ public class NombreDeJoueur {
                 }
             }
         }
+    }
+    
+    @FXML
+    private void jouer(ActionEvent event) {
+    	if (event.getSource() == unJoueur) {
+    		nbJoueur = 1;
+    	} else if (event.getSource() == deuxJoueurs){
+    		nbJoueur = 2;
+    	}
+    	
+    	ouvrirPopUp();
+    }
+    
+    private void ouvrirPopUp() {
+    	String cheminChoixDeck;
+//    	try {
+//    		
+//    	} catch (IOException e) {
+//    		e.printStackTrace();
+//    	}
+    }
+    
+    public static int getNbJoueur() {
+    	return nbJoueur;
     }
 }
