4009b2e27db58033dc4bb828f1351b1dfe03289f
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-05-27 16:23:01 +0200

    modification du controller Nombre de joueur
    mofication du fichier fxml concernant la vue du nombre de joueurs

--- src/main/java/com/sae201/timeline/controller/NombreDeJoueur.java
+++ src/main/java/com/sae201/timeline/controller/NombreDeJoueur.java
@@ -2,60 +2,79 @@ package com.sae201.timeline.controller;
 
 import static com.sae201.timeline.util.StyleUtilitaire.styleBouton;
 
+import com.sae201.timeline.util.DialogueUtilitaire;
+
+import java.io.IOException;
+
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class NombreDeJoueur {
 	private static int nbJoueur;
-    @FXML
-    private VBox root;
-    @FXML
-    private Button unJoueur, deuxJoueurs;
+	@FXML
+	private VBox root;
+	@FXML
+	private Button unJoueur, deuxJoueurs;
 
-    @FXML
-    private void initialize() {
-        Platform.runLater(this::styleButton);
-        nbJoueur = 0;
-    }
+	@FXML
+	private void initialize() {
+		Platform.runLater(this::styleButton);
+		nbJoueur = 0;
+	}
 
-    private void styleButton() {
-        for (Node node : root.getChildren()) {
-            if (node instanceof HBox hbox) {
-                for (Node hboxChild : hbox.getChildren()) {
-                    if (hboxChild instanceof Button btn) {
-                        styleBouton(btn);
-                    }
-                }
-            }
-        }
-    }
-    
-    @FXML
-    private void jouer(ActionEvent event) {
-    	if (event.getSource() == unJoueur) {
-    		nbJoueur = 1;
-    	} else if (event.getSource() == deuxJoueurs){
-    		nbJoueur = 2;
-    	}
-    	
-    	ouvrirPopUp();
-    }
-    
-    private void ouvrirPopUp() {
-    	String cheminChoixDeck;
-//    	try {
-//    		
-//    	} catch (IOException e) {
-//    		e.printStackTrace();
-//    	}
-    }
-    
-    public static int getNbJoueur() {
-    	return nbJoueur;
-    }
+	private void styleButton() {
+		for (Node node : root.getChildren()) {
+			if (node instanceof HBox hbox) {
+				for (Node hboxChild : hbox.getChildren()) {
+					if (hboxChild instanceof Button btn) {
+						styleBouton(btn);
+					}
+				}
+			}
+		}
+	}
+
+	@FXML
+	private void jouer(ActionEvent event) {
+		if (event.getSource() == unJoueur) {
+			nbJoueur = 1;
+		} else if (event.getSource() == deuxJoueurs) {
+			nbJoueur = 2;
+		}
+
+		ouvrirPopUp();
+	}
+
+	private void ouvrirPopUp() {
+		String cheminChoixDeck = "/com/sae201/timeline/choixDeck.fxml";
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(cheminChoixDeck));
+		try {
+			Scene scene = new Scene(loader.load());
+			Stage nouvelleStage = new Stage();
+			nouvelleStage.setScene(scene);
+			nouvelleStage.initModality(Modality.APPLICATION_MODAL);
+			nouvelleStage.setTitle("Choix du deck");
+			nouvelleStage.setResizable(false);
+			nouvelleStage.showAndWait();
+		} catch (IOException e) {
+			if (e.getCause() != null) {
+				DialogueUtilitaire.alerterChargementVueImpossible(e.getCause().getMessage());
+			} else {
+				DialogueUtilitaire.alerterChargementVueImpossible(e.getMessage());
+			}
+		}
+	}
+
+	public static int getNbJoueur() {
+		return nbJoueur;
+	}
 }
