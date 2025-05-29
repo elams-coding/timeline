412fb8465db7aa1f9c91400a5e8d2332c75cb7d2
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-05-29 13:45:06 +0200

    refractor: style de code

--- src/main/java/com/sae201/timeline/controller/Accueil.java
+++ src/main/java/com/sae201/timeline/controller/Accueil.java
@@ -18,104 +18,102 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Accueil {
-    public static boolean estModeSolo;
-    private FXMLLoader loader;
-    private Stage stage;
+	public static boolean estModeSolo;
+	private FXMLLoader loader;
+	private Stage stage;
 
-    @FXML
-    private VBox conteneurChoix;
+	@FXML
+	private VBox conteneurChoix;
 
-    @FXML
-    private void initialize() {
-        Platform.runLater(this::tailleBouton);
-    }
+	@FXML
+	private void initialize() {
+		Platform.runLater(this::tailleBouton);
+	}
 
-    private void tailleBouton() {
-        double largeurInitiale = conteneurChoix.getWidth();
-        for (Node node : conteneurChoix.getChildren()) {
-            if (node instanceof Button btn) {
-                btn.setMaxWidth(largeurInitiale);
-                largeurInitiale -= 10;
-                styleBouton(btn);
-            }
-        }
-    }
+	private void tailleBouton() {
+		double largeurInitiale = conteneurChoix.getWidth();
+		for (Node node : conteneurChoix.getChildren()) {
+			if (node instanceof Button btn) {
+				btn.setMaxWidth(largeurInitiale);
+				largeurInitiale -= 10;
+				styleBouton(btn);
+			}
+		}
+	}
 
-    @FXML
-    private void nouvellePartie(ActionEvent event) {
-        String cheminVueNombreDeJoueurs = "/com/sae201/timeline/nombreDeJoueurs.fxml";
-        loader = new FXMLLoader(getClass().getResource(cheminVueNombreDeJoueurs));
-        try {
-            Scene scene = new Scene(loader.load());
-            Scene sceneInitiale = ((Button) event.getSource()).getScene();
-            stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
-            stage.close();
-            stage.setScene(scene);
-            stage.setTitle("Nombre de joueurs");
-            stage.setScene(scene);
-            stage.setResizable(false);
-            stage.setOnCloseRequest(e -> {
-                e.consume();
-                retourAccueil(sceneInitiale, stage);
-            });
-            stage.show();
-        } catch (IOException e) {
-        	DialogueUtilitaire.afficherAlerte(e);
-        }
-    }
-
-    @FXML
-    private void chargerPartie() {
-        afficherFonctionnaliteNonDisponible("Charger partie",
-                "Il n'est pas possible de charger une partie pour le moment.");
-    }
-
-    @FXML
-    private void gererDecks(ActionEvent event) {
-    	// todo changer le la vue pour la gestion des decks
-        String cheminVueGererDeck = "/com/sae201/timeline/gererDeck.fxml";
-        loader = new FXMLLoader(getClass().getResource(cheminVueGererDeck));
-        try {
+	@FXML
+	private void nouvellePartie(ActionEvent event) {
+		String cheminVueNombreDeJoueurs = "/com/sae201/timeline/nombreDeJoueurs.fxml";
+		loader = new FXMLLoader(getClass().getResource(cheminVueNombreDeJoueurs));
+		try {
 			Scene scene = new Scene(loader.load());
-			stage = new Stage();
+			Scene sceneInitiale = ((Button) event.getSource()).getScene();
+			stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+			stage.close();
+			stage.setScene(scene);
+			stage.setTitle("Nombre de joueurs");
+			stage.setScene(scene);
+			stage.setResizable(false);
+			stage.setOnCloseRequest(e -> {
+				e.consume();
+				retourAccueil(sceneInitiale, stage);
+			});
+			stage.show();
+		} catch (IOException e) {
+			DialogueUtilitaire.afficherAlerte(e);
+		}
+	}
+
+	@FXML
+	private void chargerPartie() {
+		afficherFonctionnaliteNonDisponible("Charger partie",
+				"Il n'est pas possible de charger une partie pour le moment.");
+	}
+
+	@FXML
+	private void gererDecks(ActionEvent event) {
+		// todo changer le la vue pour la gestion des decks
+		String cheminVueGererDeck = "/com/sae201/timeline/gererDeck.fxml";
+		loader = new FXMLLoader(getClass().getResource(cheminVueGererDeck));
+		try {
+			Scene scene = new Scene(loader.load());
+			stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+			stage.close();
 			stage.setScene(scene);
 			stage.setResizable(false);
 			stage.setTitle("Gérer les decks");
 			Scene sceneInitiale = ((Button) event.getSource()).getScene();
 			stage.setOnCloseRequest(e -> {
-                e.consume();
-                retourAccueil(sceneInitiale, stage);
-            });
+				e.consume();
+				retourAccueil(sceneInitiale, stage);
+			});
 			stage.show();
 		} catch (IOException e) {
 			DialogueUtilitaire.afficherAlerte(e);
 		}
-    }
+	}
 
-    @FXML
-    private void reglages() {
-        afficherFonctionnaliteNonDisponible("Aucune option pour l'instant.");
+	@FXML
+	private void reglages() {
+		afficherFonctionnaliteNonDisponible("Aucune option pour l'instant.");
 
-        /*
-         * TODO faire les options de réglages :
-         * - choix de la langue,
-         * - choix du thème,
-         * - activer/désactiver le son,
-         * - activer/désactiver la musique de fond.
-         */
-    }
+		/*
+		 * TODO faire les options de réglages : - choix de la langue, - choix du thème,
+		 * - activer/désactiver le son, - activer/désactiver la musique de fond.
+		 */
+	}
 
-    @FXML
-    private void quitter() {
-        Platform.exit();
-    }
+	@FXML
+	private void quitter() {
+		Platform.exit();
+	}
 
-    private void retourAccueil(Scene sceneInitiale, Stage stage) {
-        stage.close();
-        stage.setScene(sceneInitiale);
-        stage.setTitle("Timeline");
-        stage.setResizable(true);
-        stage.setOnCloseRequest(e -> Platform.exit());
-        stage.show();
-    }
+	private void retourAccueil(Scene sceneInitiale, Stage stage) {
+		stage.close();
+		stage.setScene(sceneInitiale);
+		stage.setTitle("Timeline");
+		stage.setResizable(true);
+		stage.setOnCloseRequest(e -> Platform.exit());
+		stage.show();
+	}
 }
