e7885b66d0b36b704237d7d9a1e572634c65c335
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-05-29 16:32:33 +0200

    feat: ajout du style aux boutons de la vue

---
+++ src/main/java/com/sae201/timeline/controller/GererDeck.java
@@ -0,0 +1,31 @@
+package com.sae201.timeline.controller;
+
+import com.sae201.timeline.util.StyleUtilitaire;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+public class GererDeck {
+	@FXML
+	private StackPane root;
+	@FXML
+	private VBox conteneurBouton;
+	
+	@FXML
+	public void initialize() {
+		Platform.runLater(this::styliserBoutons);
+	}
+	
+	private void styliserBoutons() {
+		for (Node node : conteneurBouton.getChildren()) {
+			if (node instanceof Button btn) {
+				StyleUtilitaire.styleBouton(btn);
+			}
+		}
+	}
+
+}
