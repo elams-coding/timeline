3da458a64e2c05fe5bd3c75564797dd547ae6288
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-06-01 15:55:16 +0200

    feat: affichage du temps choisi dans le label et ajout d'ombre et glow sur le choicebox

---
+++ src/main/java/com/sae201/timeline/controller/ChoixDuDeck.java
@@ -0,0 +1,44 @@
+package com.sae201.timeline.controller;
+
+import com.sae201.timeline.objet.Deck;
+import com.sae201.timeline.util.StyleUtilitaire;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.VBox;
+
+public class ChoixDuDeck {
+	private static int temps;
+	
+	@FXML
+	private VBox root;
+	@FXML
+	private ChoiceBox<Deck> choixDeck;
+	@FXML
+	private Slider sliderTemps;
+	@FXML
+	private Label labelTemps;
+	
+	@FXML
+	public void initialize() {
+		Platform.runLater(() -> StyleUtilitaire.effetOmbreEtGlow(choixDeck));
+		
+		int valeurInitial = (int) sliderTemps.getValue();
+		temps = valeurInitial;
+		labelTemps.setText(String.format("%02d", valeurInitial));
+		
+		sliderTemps.valueProperty().addListener((obs, oldVal, newVal) -> {
+			int valeur = newVal.intValue();
+	        temps = valeur;
+	        System.out.println(temps);
+	        labelTemps.setText(String.format("%02d", valeur));
+		});
+	}
+	
+	public static int getTemps() {
+		return temps;
+	}
+}
