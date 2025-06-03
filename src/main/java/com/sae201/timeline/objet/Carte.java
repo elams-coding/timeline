e89f6c0dab74b2000237e7591f02fdd6bde4e358
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-06-03 08:42:57 +0200

    changement de la visibilité des attributs

--- src/main/java/com/sae201/timeline/objet/Carte.java
+++ src/main/java/com/sae201/timeline/objet/Carte.java
@@ -1,35 +1,8 @@
 package com.sae201.timeline.objet;
 
-import java.time.YearMonth;
-
-import javafx.scene.image.Image;
-
 public class Carte {
-    private String titre;
-    private YearMonth date;
-    private Image image;
-    private String informations;
-    
-    public Carte(String titre, YearMonth date, Image image, String informations) {
-    	this.titre = titre;
-    	this.date = date;
-    	this.image = image;
-    	this.informations = informations;
-    }
-
-	public String getTitre() {
-		return titre;
-	}
-
-	public YearMonth getDate() {
-		return date;
-	}
-
-	public Image getImage() {
-		return image;
-	}
-
-	public String getInformations() {
-		return informations;
-	}
+    public String titre;
+    public String informations;
+    public String date;
+    public String urlImage;
 }
--- src/main/java/com/sae201/timeline/objet/Deck.java
+++ src/main/java/com/sae201/timeline/objet/Deck.java
@@ -1,14 +1,6 @@
 package com.sae201.timeline.objet;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Deck {
-	private List<Carte> collectionCarte;
-	private String nom;
-	
-	public Deck(String nom) {
-		this.nom = nom;
-		collectionCarte = new ArrayList<>();
-	}
+	public String nom;
+	public Carte[] cartes;
 }
