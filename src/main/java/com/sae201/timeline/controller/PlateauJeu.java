1e137489f225fbc35f64333cd149c5e7bfb502f2
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-06-05 19:54:29 +0200

    afficher le temps si le jeu est configuré pour deux joueurs

--- src/main/java/com/sae201/timeline/controller/PlateauJeu.java
+++ src/main/java/com/sae201/timeline/controller/PlateauJeu.java
@@ -1,9 +1,14 @@
 package com.sae201.timeline.controller;
 
+import com.sae201.timeline.util.StyleUtilitaire;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 
 public class PlateauJeu {
+	@FXML
+	private Label temps;
 	@FXML
 	private HBox plateau;
 	@FXML
@@ -11,6 +16,11 @@ public class PlateauJeu {
 	
 	@FXML
 	private void initialize() {
-		
+		if (NombreDeJoueur.nbJoueur == 1) {
+			StyleUtilitaire.cacherNode(temps);
+		} else {
+			String string = Integer.toString(ChoixDuDeck.temps);
+			temps.setText(string);
+		}
 	}
 }
