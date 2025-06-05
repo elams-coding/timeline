41cb8ce95b652e36d43c057b08f2eea3fee05169
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-06-05 20:20:46 +0200

    ajout du timer

--- src/main/java/com/sae201/timeline/controller/PlateauJeu.java
+++ src/main/java/com/sae201/timeline/controller/PlateauJeu.java
@@ -2,9 +2,12 @@ package com.sae201.timeline.controller;
 
 import com.sae201.timeline.util.StyleUtilitaire;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.util.Duration;
 
 public class PlateauJeu {
 	@FXML
@@ -13,14 +16,32 @@ public class PlateauJeu {
 	private HBox plateau;
 	@FXML
 	private HBox main;
-	
+
+	private int tempsRestant; // Temps restant en secondes
+	private Timeline timer;
+
 	@FXML
 	private void initialize() {
 		if (NombreDeJoueur.nbJoueur == 1) {
 			StyleUtilitaire.cacherNode(temps);
 		} else {
-			String string = Integer.toString(ChoixDuDeck.temps);
-			temps.setText(string);
+			tempsRestant = ChoixDuDeck.temps; // Initialisation du temps
+			temps.setText(Integer.toString(tempsRestant));
+			lancerTimer();
 		}
+	}
+
+	private void lancerTimer() {
+		timer = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+			tempsRestant--;
+			temps.setText(Integer.toString(tempsRestant));
+
+			if (tempsRestant <= 0) {
+				timer.stop();
+				temps.setText("Temps écoulé !");
+			}
+		}));
+		timer.setCycleCount(Timeline.INDEFINITE);
+		timer.play();
 	}
 }
